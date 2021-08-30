3aec5722f5b801ee999b9af6b16a442139b0bab0
Luke Hinton <lukejohnhinton@gmail.com>
2021-08-30 17:35:38 +0100

    Finished App.
    Application finished.

--- ChargeCar/View/MarkerView.swift
+++ ChargeCar/View/MarkerView.swift
@@ -14,21 +14,13 @@ class MarkerView: MKAnnotationView {
             if (newValue as? Charger) != nil {
                 canShowCallout = true
                 rightCalloutAccessoryView = UIButton(type: .infoLight)
-                let pinImage = UIImage(named: "PublicAnnotation")
-                let size = CGSize(width: 50, height: 50)
-                UIGraphicsBeginImageContext(size)
-                pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-                let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-                image = resizedImage
+                let pinImage = UIImage(named: "publicannotation50")
+                image = pinImage
             } else if (newValue as? PrivateChargerMap != nil) {
                 canShowCallout = true
                 rightCalloutAccessoryView = UIButton(type: .infoLight)
-                let pinImage = UIImage(named: "HomeAnnotation")
-                let size = CGSize(width: 50, height: 50)
-                UIGraphicsBeginImageContext(size)
-                pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-                let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-                image = resizedImage
+                let pinImage = UIImage(named: "privateannotation50")
+                image = pinImage
             } else {
                 return
             }
